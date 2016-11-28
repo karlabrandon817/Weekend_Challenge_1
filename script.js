0ef4b3d44549ec686698c40c20ddd170f3582c46
Karla Hugo <karlabrandon817@gmail.com>
2016-11-28 10:24:33 -0600

    adjusted input field

--- script.js
+++ script.js
@@ -12,21 +12,21 @@ console.log("job title:", jobTitle);
 var annualSalary = document.getElementById('annualSalary').value;
 console.log("salary:", annualSalary);
 
+
 //add employee to array
 newEmp=[firstName, lastName, empId, jobTitle, annualSalary];
 console.log(newEmp);
 employeeArray.push(newEmp);
 
-
 //add new employee info to DOM
 document.getElementById("allEmployees").innerHTML+=("<tr><td>"+ firstName +"</td><td>" + lastName + "</td><td>"+ empId + "</td><td>" + jobTitle + "</td><td>" +
-annualSalary +"</tr></td>");
+annualSalary +"</td></td>");
 
 linebreak = document.createElement("br");
 monthlySalary.appendChild(linebreak);
-  document.getElementById("monthlySalary").innerHTML = ("Montly Salary Total: $" + Math.round(monthlySalaryTotal()*100)/100);
-
+document.getElementById("monthlySalary").innerHTML = ("Montly Salary Total: $" + Math.round(monthlySalaryTotal()*100)/100);
 };
+
 
 //reset form with 'submit' button click
 function resetForm() {
@@ -41,5 +41,3 @@ var monthlySalaryTotal = function () {
 }
   return salariesTotal/12;
 };
-
-//delete button
