8b786ed8d8d11fca03000206c8497837233a753d
Karla Hugo <karlabrandon817@gmail.com>
2016-11-29 14:26:23 -0600

    read me

---
+++ readme.md
@@ -0,0 +1,19 @@
+Description
+Weekend Challenge 1
+For your weekend challenge, you will need to create an application that records employees along with their salary. We also want to add the salaries up so we know how much we’re spending each month.
+
+The application should first have an input form that collects the following:
+
+Employee First Name
+
+Employee Last Name
+
+ID Number
+
+Job Title
+
+Annual Salary
+
+A 'Submit' button should clear out the inputs and your logic should store that information. Then, that information should be appended to the DOM so the user of the application can see the information they just entered.
+
+Finally, your logic should calculate all of the employee salaries and report back the Monthly cost of salaries.
--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 var employeeArray = [];
 
-var userInput = function () {
-var firstName = document.getElementById('firstName').value;
-console.log("first name:", firstName);
-var lastName = document.getElementById('lastName').value;
-console.log("last name:", lastName);
-var empId = document.getElementById('empId').value;
-console.log("ID:", empId);
-var jobTitle = document.getElementById('jobTitle').value;
-console.log("job title:", jobTitle);
-var annualSalary = document.getElementById('annualSalary').value;
-console.log("salary:", annualSalary);
+var userInput = function() {
+    var firstName = document.getElementById('firstName').value;
+    console.log("first name:", firstName);
+    var lastName = document.getElementById('lastName').value;
+    console.log("last name:", lastName);
+    var empId = document.getElementById('empId').value;
+    console.log("ID:", empId);
+    var jobTitle = document.getElementById('jobTitle').value;
+    console.log("job title:", jobTitle);
+    var annualSalary = document.getElementById('annualSalary').value;
+    console.log("salary:", annualSalary);
 
 
-//add employee to array
-newEmp=[firstName, lastName, empId, jobTitle, annualSalary];
-console.log(newEmp);
-employeeArray.push(newEmp);
+    //add employee to array
+    newEmp = [firstName, lastName, empId, jobTitle, annualSalary];
+    console.log(newEmp);
+    employeeArray.push(newEmp);
 
-//add new employee info to DOM
-document.getElementById("allEmployees").innerHTML+=("<tr><td>"+ firstName +"</td><td>" + lastName + "</td><td>"+ empId + "</td><td>" + jobTitle + "</td><td>" +
-annualSalary +"</td></td>");
+    //add new employee info to DOM
+    document.getElementById("allEmployees").innerHTML += ("<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + empId + "</td><td>" + jobTitle + "</td><td>" +
+        annualSalary + "</td></td>");
 
-linebreak = document.createElement("br");
-monthlySalary.appendChild(linebreak);
-document.getElementById("monthlySalary").innerHTML = ("Montly Salary Total: $" + Math.round(monthlySalaryTotal()*100)/100);
+    linebreak = document.createElement("br");
+    monthlySalary.appendChild(linebreak);
+    document.getElementById("monthlySalary").innerHTML = ("Montly Salary Total: $" + Math.round(monthlySalaryTotal() * 100) / 100);
 };
 
 
 //reset form with 'submit' button click
 function resetForm() {
-document.getElementById("empInfoForm").reset();
+    document.getElementById("empInfoForm").reset();
 }
 
 //function for monthly cost of salaries
-var monthlySalaryTotal = function () {
-  var salariesTotal = 0;
-  for (var i = 0; i < employeeArray.length; i++){
-  salariesTotal += Number(employeeArray[i][4]);
-}
-  return salariesTotal/12;
+var monthlySalaryTotal = function() {
+    var salariesTotal = 0;
+    for (var i = 0; i < employeeArray.length; i++) {
+        salariesTotal += Number(employeeArray[i][4]);
+    }
+    return salariesTotal / 12;
 };
