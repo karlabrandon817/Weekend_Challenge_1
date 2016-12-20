6b1701391c11642fa0663386250676e244d2f216
Karla Hugo <karlabrandon817@gmail.com>
2016-12-20 11:02:48 -0600

    added jquery and udpated syntax

--- script.js
+++ script.js
@@ -1,43 +1,44 @@
-var employeeArray = [];
+$(document).ready(function(){
+  console.log('George the cat');
 
-var userInput = function() {
-    var firstName = document.getElementById('firstName').value;
-    console.log("first name:", firstName);
-    var lastName = document.getElementById('lastName').value;
-    console.log("last name:", lastName);
-    var empId = document.getElementById('empId').value;
-    console.log("ID:", empId);
-    var jobTitle = document.getElementById('jobTitle').value;
-    console.log("job title:", jobTitle);
-    var annualSalary = document.getElementById('annualSalary').value;
-    console.log("salary:", annualSalary);
+  var employeeArray = [];
 
-
+  var userInput = function() {
+    var firstName = $('#firstName').val();
+    var lastName = $('#lastName').val();
+    var empId = $('#empId').val();
+    var jobTitle = $('#jobTitle').val();
+    var annualSalary = $('#annualSalary').val();
     //add employee to array
     newEmp = [firstName, lastName, empId, jobTitle, annualSalary];
-    console.log(newEmp);
     employeeArray.push(newEmp);
-
-    //add new employee info to DOM
-    document.getElementById("allEmployees").innerHTML += ("<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + empId + "</td><td>" + jobTitle + "</td><td>" +
-        annualSalary + "</td></td>");
-
-    linebreak = document.createElement("br");
-    monthlySalary.appendChild(linebreak);
-    document.getElementById("monthlySalary").innerHTML = ("Montly Salary Total: $" + Math.round(monthlySalaryTotal() * 100) / 100);
-};
+  };//end userInput function
 
 
-//reset form with 'submit' button click
-function resetForm() {
-    document.getElementById("empInfoForm").reset();
-}
+  //add new employee info to DOM
+  $('#submitButton').on('click', function(){
+    userInput();
+    $('#empInfoForm').find('input').val('');//clear form after submit
+    var outputText = '';
+    for(var i = 0; i < employeeArray.length; i++){
+    outputText = ("<tr><td>" + employeeArray[i][0] + "</td><td>" + employeeArray[i][1] + "</td><td>" + employeeArray[i][2] + "</td><td>" + employeeArray[i][3] + "</td><td>" +
+    employeeArray[i][4] + "</td><td>" + "<button class='deleteEmployeeButton'>Delete</button>" + "</td></tr>");
+
+
+    monthlySalaryTotal();
+  }//end for loop
+    $('#allEmployees').append(outputText);
+    $('#monthlySalary').html("Montly Salary Total: " + Math.round((monthlySalaryTotal() * 100) / 100).toLocaleString('en-US', { style: 'currency', currency: 'USD' }));
+} );//end submitButton onClick function
+
 
 //function for monthly cost of salaries
-var monthlySalaryTotal = function() {
+  var monthlySalaryTotal = function() {
     var salariesTotal = 0;
     for (var i = 0; i < employeeArray.length; i++) {
         salariesTotal += Number(employeeArray[i][4]);
     }
     return salariesTotal / 12;
-};
+  };//end monthlySalary
+
+});//end doc ready
